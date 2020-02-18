901309ad41ceaa75014aa37d5a2f194b718437e4
lesina <lesha_lesovoy@mail.ru>
2020-02-18 21:46:42 +0300

    added user commands, added games, can now translate even if its not in correct chat, now can rename chats

--- InternationalBot.py
+++ InternationalBot.py
@@ -5,10 +5,10 @@ import os
 import discord
 import translationAPI
 
-template_channel = "general"
 bot_token = os.environ["BOT_TOKEN"]
 yandex_api_token = os.environ["YANDEX_API_TOKEN"]
-command_symbol = "!"
+admin_command_symbol = "!"
+user_command_symbol = "/"
 
 
 class MyClient(discord.Client):
@@ -26,6 +26,34 @@ class MyClient(discord.Client):
             text = text[:start_index] + str(author).split('#')[0] + text[end_index+1:]
         return text
 
+    def src_language_is_correct(self, src, message):
+        for dest in translationAPI.allowed_languages:
+            if src == dest:
+                continue
+                
+            translated_text = ""
+                
+            if self.choosen_translater == 0:
+                translated_text = self.google_translator.translate(message.content, src=src, dest=dest).text
+            elif self.choosen_translater == 1:
+                self.yandex_translator.set_from_lang(src)
+                self.yandex_translator.set_to_lang(dest)
+                self.yandex_translator.set_text(message.content)
+                translated_text = self.yandex_translator.translate()
+                    
+            if message.content == translated_text:
+                return False
+            else:
+                return True
+
+###=============================================
+###     ADMIN COMMANDS
+###=============================================
+
+    def handle_admin_commands(self, message):
+        if message.content.startswith(admin_command_symbol) and len(message.content) > 2 and message.content.split()[0][1:] in self.admin_commands and message.author.guild_permissions.administrator:
+            await self.admin_commands[message.content.split()[0][1:]](self, message.channel)
+    
     async def change_translator(self, channel):
         self.choosen_translater = (self.choosen_translater + 1) % len(self.translators_list)
         await channel.send('Switched to **{0}**'.format(self.translators_list[self.choosen_translater]))
@@ -33,10 +61,10 @@ class MyClient(discord.Client):
     async def translator_info(self, channel):
         await channel.send('I am using now **{0}**'.format(self.translators_list[self.choosen_translater]))
         
-    async def help(self, channel):
-        message_text = "Availables commands: \n"
-        for command in self.commands.keys():
-            message_text += "*{0}* \n".format(command)
+    async def admin_help(self, channel):
+        message_text = "Available admin commands: \n"
+        for command in self.admin_commands.keys():
+            message_text += "*{0}{1}* \n".format(admin_command_symbol, command)
         await channel.send(message_text)
         
     async def enable_bot(self, channel):
@@ -47,44 +75,83 @@ class MyClient(discord.Client):
         self.is_enabled = False
         await channel.send("OK =(")
         
-    commands = {
-        "help" : help,
+    admin_commands = {
+        "help" : admin_help,
         "change_translator" : change_translator,
         "translator_info" :  translator_info,
         "disable_bot" : disable_bot,
         "enable_bot" : enable_bot
     }
 
+###=============================================
+###     USER COMMANDS
+###=============================================
+
+    playing_ping_pong = False
+
+    def handle_user_commands(self, message):
+        if message.content.startswith(user_command_symbol) and len(message.content) > 2 and message.content.split()[0][1:] in self.user_commands:
+            await self.user_commands[message.content.split()[0][1:]](self, message)
+        
+        if self.playing_ping_pong:
+            await play_ping_pong(self, message)
+        
+    async def admin_help(self, message):
+        message_text = "Available user commands: \n"
+        for command in self.user_commands.keys():
+            message_text += "*{0}{1}* \n".format(user_command_symbol, command)
+        await message.channel.send(message_text)
+        
+    async def play_ping_pong(self, message):
+        if not self.playing_ping_pong or message.content.lower() == "pong":
+            self.playing_ping_pong = True
+            message_text = "Ping"
+        else:
+            self.playing_ping_pong = False
+            message_text = "I win!"
+        
+        await message.channel.send(message_text)
+        
+    async def stop(self, message):
+        self.playing_ping_pong = False
+        await message.channel.send("Ok =(")
+        
+    user_commands = {
+        "help" : admin_help,
+        "playpingpong" : play_ping_pong,
+        "stop" : stop
+    }
+    
+###=============================================
+###     API CALLBACKS
+###=============================================
+
     async def on_ready(self):
         self.yandex_translator.set_key(yandex_api_token)
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
-        if message.content.startswith(command_symbol) and len(message.content) > 2 and message.content.split()[0][1:] in self.commands and message.author.guild_permissions.administrator:
-            await self.commands[message.content.split()[0][1:]](self, message.channel)
+        handle_admin_commands(message)
+        handle_user_commands(message)
 
-        channel_name = str(message.channel)
-        if message.author == self.user or not channel_name.startswith(template_channel):
+        channel_id = message.channel.id
+        if message.author == self.user or not channel_id in translationAPI.channel_id_to_language.keys():
             return
 
          if not self.is_enabled:
             return
             
-        if channel_name == template_channel:
-            src = "ru"
-        else:
-            src = channel_name.split('-')[-1]
+        src = translationAPI.channel_id_to_language[channel_id]
+        language_is_known = src_language_is_correct(src, message)
 
         for dest in translationAPI.allowed_languages:
-            if dest == "":
-                dest = "ru"
-				
             if src == dest:
                 continue
+                
             channel_to_send = None
 
             for channel in self.get_all_channels():
-                if str(channel) == "{0}-{1}".format(template_channel, dest) or (str(channel) == template_channel and dest == "ru"):
+                if channel.id == translationAPI.language_to_channel_id[dest]:
                     channel_to_send = channel
                     break
 
@@ -96,7 +163,8 @@ class MyClient(discord.Client):
                 if self.choosen_translater == 0:
                     translated_text = self.google_translator.translate(message.content, src=src, dest=dest).text
                 elif self.choosen_translater == 1:
-                    self.yandex_translator.set_from_lang(src)
+                    if language_is_known:
+                        self.yandex_translator.set_from_lang(src)
                     self.yandex_translator.set_to_lang(dest)
                     self.yandex_translator.set_text(message.content)
                     translated_text = self.yandex_translator.translate()
--- translationAPI.py
+++ translationAPI.py
@@ -1,5 +1,21 @@
+RU = "ru"
+EN = "en"
+DE = "de"
+
+RU_ID = 653652629515272195
+EN_ID = 658221821119954945
+DE_ID = 658224926318264321
+
 allowed_languages = [
-    "",
-    "en",
-    "de",
+    RU,
+    EN,
+    DE,
 ]
+
+language_to_channel_id = {
+    RU: RU_ID,
+    EN: EN_ID,
+    DE: DE_ID,
+}
+
+channel_id_to_language = dict([(value, key) for key, value in language_to_channel_id.items()]) 
